cee349bbb14c4b336580a682b9deef9c7a8d802e
Roman Kozlov <romanko@romanko-mac.local>
2017-09-11 11:01:31 +0300

    update formatting

--- lib/android/app/src/test/java/com/reactnativenavigation/viewcontrollers/ContainerViewControllerTest.java
+++ lib/android/app/src/test/java/com/reactnativenavigation/viewcontrollers/ContainerViewControllerTest.java
@@ -3,17 +3,12 @@ package com.reactnativenavigation.viewcontrollers;
 import android.app.Activity;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.widget.TextView;
 
 import com.reactnativenavigation.BaseTest;
 import com.reactnativenavigation.mocks.TestContainerView;
 import com.reactnativenavigation.parse.NavigationOptions;
 
 import org.junit.Test;
-
-import java.lang.reflect.Field;
 
 import static org.assertj.core.api.Java6Assertions.assertThat;
 import static org.mockito.Mockito.spy;
@@ -22,145 +17,144 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 public class ContainerViewControllerTest extends BaseTest {
+	private Activity activity;
+	private ContainerViewController uut;
+	private ContainerViewController.ContainerView view;
+	private NavigationOptions initialNavigationOptions;
 
-    private Activity activity;
-    private ContainerViewController uut;
-    private ContainerViewController.ContainerView view;
-    private NavigationOptions initialNavigationOptions;
+	@Override
+	public void beforeEach() {
+		super.beforeEach();
+		activity = newActivity();
+		initialNavigationOptions = new NavigationOptions();
+		view = spy(new TestContainerView(activity));
+		uut = new ContainerViewController(activity, "containerId1", "containerName", new ContainerViewController.ContainerViewCreator() {
+			@Override
+			public ContainerViewController.ContainerView create(final Activity activity1, final String containerId, final String containerName) {
+				return view;
+			}
+		}, initialNavigationOptions);
+	}
 
-    @Override
-    public void beforeEach() {
-        super.beforeEach();
-        activity = newActivity();
-        initialNavigationOptions = new NavigationOptions();
-        view = spy(new TestContainerView(activity));
-        uut = new ContainerViewController(activity, "containerId1", "containerName", new ContainerViewController.ContainerViewCreator() {
-            @Override
-            public ContainerViewController.ContainerView create(final Activity activity1, final String containerId, final String containerName) {
-                return view;
-            }
-        }, initialNavigationOptions);
-    }
+	@Test
+	public void createsViewFromContainerViewCreator() throws Exception {
+		assertThat(uut.getView()).isSameAs(view);
+	}
 
-    @Test
-    public void createsViewFromContainerViewCreator() throws Exception {
-        assertThat(uut.getView()).isSameAs(view);
-    }
+	@Test
+	public void containerViewDestroyedOnDestroy() throws Exception {
+		uut.ensureViewIsCreated();
+		verify(view, times(0)).destroy();
+		uut.destroy();
+		verify(view, times(1)).destroy();
+	}
 
-    @Test
-    public void containerViewDestroyedOnDestroy() throws Exception {
-        uut.ensureViewIsCreated();
-        verify(view, times(0)).destroy();
-        uut.destroy();
-        verify(view, times(1)).destroy();
-    }
+	@Test
+	public void lifecycleMethodsSentToContainerView() throws Exception {
+		uut.ensureViewIsCreated();
+		verify(view, times(0)).sendContainerStart();
+		verify(view, times(0)).sendContainerStop();
+		uut.onViewAppeared();
+		verify(view, times(1)).sendContainerStart();
+		verify(view, times(0)).sendContainerStop();
+		uut.onViewDisappear();
+		verify(view, times(1)).sendContainerStart();
+		verify(view, times(1)).sendContainerStop();
+	}
 
-    @Test
-    public void lifecycleMethodsSentToContainerView() throws Exception {
-        uut.ensureViewIsCreated();
-        verify(view, times(0)).sendContainerStart();
-        verify(view, times(0)).sendContainerStop();
-        uut.onViewAppeared();
-        verify(view, times(1)).sendContainerStart();
-        verify(view, times(0)).sendContainerStop();
-        uut.onViewDisappear();
-        verify(view, times(1)).sendContainerStart();
-        verify(view, times(1)).sendContainerStop();
-    }
+	@Test
+	public void isViewShownOnlyIfContainerViewIsReady() throws Exception {
+		assertThat(uut.isViewShown()).isFalse();
+		uut.ensureViewIsCreated();
+		when(view.asView().isShown()).thenReturn(true);
+		assertThat(uut.isViewShown()).isFalse();
+		when(view.isReady()).thenReturn(true);
+		assertThat(uut.isViewShown()).isTrue();
+	}
 
-    @Test
-    public void isViewShownOnlyIfContainerViewIsReady() throws Exception {
-        assertThat(uut.isViewShown()).isFalse();
-        uut.ensureViewIsCreated();
-        when(view.asView().isShown()).thenReturn(true);
-        assertThat(uut.isViewShown()).isFalse();
-        when(view.isReady()).thenReturn(true);
-        assertThat(uut.isViewShown()).isTrue();
-    }
+	@Test
+	public void applyNavigationOptionsHandlesNoParentStack() throws Exception {
+		assertThat(uut.getParentStackController()).isNull();
+		uut.onViewAppeared();
+		assertThat(uut.getParentStackController()).isNull();
+	}
 
-    @Test
-    public void applyNavigationOptionsHandlesNoParentStack() throws Exception {
-        assertThat(uut.getParentStackController()).isNull();
-        uut.onViewAppeared();
-        assertThat(uut.getParentStackController()).isNull();
-    }
+	@Test
+	public void initialOptionsAppliedOnAppear() throws Exception {
+		assertThat(uut.getNavigationOptions()).isSameAs(initialNavigationOptions);
+		initialNavigationOptions.title = "the title";
+		StackController stackController = new StackController(activity, "stackId");
+		stackController.push(uut);
+		assertThat(stackController.getTopBar().getTitle()).isEmpty();
 
-    @Test
-    public void initialOptionsAppliedOnAppear() throws Exception {
-        assertThat(uut.getNavigationOptions()).isSameAs(initialNavigationOptions);
-        initialNavigationOptions.title = "the title";
-        StackController stackController = new StackController(activity, "stackId");
-        stackController.push(uut);
-        assertThat(stackController.getTopBar().getTitle()).isEmpty();
+		uut.onViewAppeared();
+		assertThat(stackController.getTopBar().getTitle()).isEqualTo("the title");
+	}
 
-        uut.onViewAppeared();
-        assertThat(stackController.getTopBar().getTitle()).isEqualTo("the title");
-    }
+	@Test
+	public void mergeNavigationOptionsUpdatesCurrentOptions() throws Exception {
+		assertThat(uut.getNavigationOptions().title).isEmpty();
+		NavigationOptions options = new NavigationOptions();
+		options.title = "new title";
+		uut.mergeNavigationOptions(options);
+		assertThat(uut.getNavigationOptions().title).isEqualTo("new title");
+		assertThat(uut.getNavigationOptions()).isSameAs(initialNavigationOptions);
+	}
 
-    @Test
-    public void mergeNavigationOptionsUpdatesCurrentOptions() throws Exception {
-        assertThat(uut.getNavigationOptions().title).isEmpty();
-        NavigationOptions options = new NavigationOptions();
-        options.title = "new title";
-        uut.mergeNavigationOptions(options);
-        assertThat(uut.getNavigationOptions().title).isEqualTo("new title");
-        assertThat(uut.getNavigationOptions()).isSameAs(initialNavigationOptions);
-    }
+	@Test
+	public void reappliesOptionsOnMerge() throws Exception {
+		StackController stackController = new StackController(activity, "stackId");
+		stackController.push(uut);
+		assertThat(stackController.getTopBar().getTitle()).isEmpty();
 
-    @Test
-    public void reappliesOptionsOnMerge() throws Exception {
-        StackController stackController = new StackController(activity, "stackId");
-        stackController.push(uut);
-        assertThat(stackController.getTopBar().getTitle()).isEmpty();
+		NavigationOptions opts = new NavigationOptions();
+		opts.title = "the new title";
+		uut.mergeNavigationOptions(opts);
 
-        NavigationOptions opts = new NavigationOptions();
-        opts.title = "the new title";
-        uut.mergeNavigationOptions(opts);
+		assertThat(stackController.getTopBar().getTitle()).isEqualTo("the new title");
+	}
 
-        assertThat(stackController.getTopBar().getTitle()).isEqualTo("the new title");
-    }
+	@Test
+	public void appliesTopBackBackgroundColor() throws Exception {
+		StackController stackController = new StackController(activity, "stackId");
+		stackController.push(uut);
+		assertThat(((ColorDrawable) stackController.getTopBar().getBackground()).getColor()).isNotEqualTo(0xffff0000);
 
-    @Test
-    public void appliesTopBackBackgroundColor() throws Exception {
-        StackController stackController = new StackController(activity, "stackId");
-        stackController.push(uut);
-        assertThat(((ColorDrawable) stackController.getTopBar().getBackground()).getColor()).isNotEqualTo(0xffff0000);
+		NavigationOptions opts = new NavigationOptions();
+		opts.topBarBackgroundColor = Color.RED;
+		uut.mergeNavigationOptions(opts);
 
-        NavigationOptions opts = new NavigationOptions();
-        opts.topBarBackgroundColor = Color.RED;
-        uut.mergeNavigationOptions(opts);
+		assertThat(((ColorDrawable) stackController.getTopBar().getBackground()).getColor()).isEqualTo(0xffff0000);
+	}
 
-        assertThat(((ColorDrawable) stackController.getTopBar().getBackground()).getColor()).isEqualTo(0xffff0000);
-    }
+	@Test
+	public void appliesTopBarTextColor() throws Exception {
+	  assertThat(uut.getNavigationOptions()).isSameAs(initialNavigationOptions);
+	  initialNavigationOptions.title = "the title";
+	  StackController stackController = new StackController(activity, "stackId");
+	  stackController.push(uut);
+	  uut.onViewAppeared();
+	  assertThat(getTitleTextView(stackController.getTopBar().getToolbar()).getCurrentTextColor()).isNotEqualTo(Color.RED);
 
-    @Test
-    public void appliesTopBarTextColor() throws Exception {
-        assertThat(uut.getNavigationOptions()).isSameAs(initialNavigationOptions);
-        initialNavigationOptions.title = "the title";
-        StackController stackController = new StackController(activity, "stackId");
-        stackController.push(uut);
-        uut.onViewAppeared();
-        assertThat(getTitleTextView(stackController.getTopBar().getToolbar()).getCurrentTextColor()).isNotEqualTo(Color.RED);
+	  NavigationOptions opts = new NavigationOptions();
+	  opts.topBarTextColor = Color.RED;
+	  uut.mergeNavigationOptions(opts);
 
-        NavigationOptions opts = new NavigationOptions();
-        opts.topBarTextColor = Color.RED;
-        uut.mergeNavigationOptions(opts);
+	  assertThat(getTitleTextView(stackController.getTopBar().getToolbar()).getCurrentTextColor()).isEqualTo(Color.RED);
+	}
 
-        assertThat(getTitleTextView(stackController.getTopBar().getToolbar()).getCurrentTextColor()).isEqualTo(Color.RED);
-    }
+  private TextView getTitleTextView(Toolbar toolbar) {
+      try {
+          Class<?> toolbarClass = Toolbar.class;
+          Field titleTextViewField = toolbarClass.getDeclaredField("mTitleTextView");
+          titleTextViewField.setAccessible(true);
 
-    private TextView getTitleTextView(Toolbar toolbar) {
-        try {
-            Class<?> toolbarClass = Toolbar.class;
-            Field titleTextViewField = toolbarClass.getDeclaredField("mTitleTextView");
-            titleTextViewField.setAccessible(true);
-
-            return (TextView) titleTextViewField.get(toolbar);
-        } catch (NoSuchFieldException e) {
-            System.out.print("no such file");
-        } catch (IllegalAccessException e) {
-            System.out.print("illegal access");
-        }
-        return null;
-    }
+          return (TextView) titleTextViewField.get(toolbar);
+      } catch (NoSuchFieldException e) {
+          System.out.print("no such file");
+      } catch (IllegalAccessException e) {
+          System.out.print("illegal access");
+      }
+      return null;
+  }
 }
